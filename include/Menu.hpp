7ec4b44fbd5912ce3f1b7145fd5bea67546d8ec8
Hevan-Viscencio <143463643+Hevan-Viscencio@users.noreply.github.com>
2023-12-01 00:59:06 +0000

    Terminar de agregar carpetas, empezar el diseño

---
+++ include/Menu.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+class Menu
+{
+private:
+    
+public:
+    Menu(){
+
+    cout << "Proyecto Hecho por: \nHevan Jesus Viscencio Lopez ----22310198 \nGael Alexandro Silva Huacuja ---22310258\n\n\n ";
+	cout << "\nCalculo de las raices de una funcion aplicando el metodo de Newton - Raphson\n";
+	cout << "\nUsaremos Esta Funcion F(X)=  pow(x,3)+sqrt(x)-6";
+	cout << "\nDonde su derivada seria F'(X)=  3*pow(x,2)+1/(2*sqrt(x))n";
+	cout << "\nIngrese el intervalo inicial (Ejemplo 3,-3):" << endl;
+
+    }
+    ~Menu() {}
+};
+
+
+
+
+    
